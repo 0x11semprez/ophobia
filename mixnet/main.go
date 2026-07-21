46c9a608d15e288e7fe038d0414a7618cf716318
0x11semprez <traoresemprez@icloud.com>
2026-07-21 19:23:57 +0200

    feat: starting creating all nodes

--- mixnet/main.go
+++ mixnet/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"crypto/rsa"
 	"mixnet/informations"
+	"mixnet/network"
 )
 
 func main() {
 	var NodesNumber int
+	var BasePort int
+
+
 	informations.AskNodes(NodesNumber)
+	informations.AskBasePort(BasePort)
+
+	nodes := make([]network.Node, NodesNumber)
+
+	for i := 0; i <= NodesNumber; i++ {
+		i.append(nodes, Node{
+			ID: i,
+			Address: BasePort + i
+			PrivateKey: 
+			PublicKey:
+		})
+	}
 }
